836f432ed3686959d18fa8e2536b03651eafd728
Ujjwal Sharma <ujjwalsharma@protonmail.com>
2018-02-07 12:50:54 +0100

    Basic openMP functionality now works

--- OpenMP/main.c
+++ OpenMP/main.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include<time.h>
+#include "omp.h"
 
-// #define RAND_MIN 1
 // #define RAND_MAX 10
+#define BILLION 1E9
 
-void test(double *a){
-  printf("I was called");
+void print(double *a){
   for (int i=0;i<10;i++){
     printf("%f,",a[i]);
   }
@@ -21,16 +21,53 @@ void multimult(double *a, double *b, int len, int steps)
     for (int i = 0; i < len; i++)
     {
       c = a[i] * b[i];
-      //a[i] = c * (double) t;
+      a[i] = c * (double) t;
     }
   }
 }
 
-void main(){
+void multimult_OMP(double *a, double *b, int len, int steps)
+{
+  double c;
+  int i = 0;
+  for (int t=0; t < steps; t++)
+  {
+    # pragma omp parallel for shared(t,len,a,b) private(i,c)
+    for (i = 0; i < len; i++)
+    {
+      c = a[i] * b[i];
+      a[i] = c * (double) t;
+    }
+  }
+}
 
+void main(int argc, char** argv){
+
+// Random Number Generator Seed
 srand(time(NULL));
 
-int LEN = 10;
+// For clock_gettime functionality
+struct timespec requestStart, requestEnd;
+
+// Length of A and B integer arrays
+
+if (argc < 3){
+  printf("\n Invalid Parameter Specification. Exiting! \n");
+  exit(2);
+}
+
+// Test Parameters
+int LEN = atoi(argv[1]);
+int NUM_THREADS = atoi(argv[2]);
+int STEPS = atoi(argv[3]);
+
+// Set Number of OMP NUM_THREADS
+omp_set_num_threads(NUM_THREADS);
+
+// Display Test Parameters
+printf("LEN = %d \n",LEN);
+printf("NUM_THREADS = %d \n",NUM_THREADS);
+printf("STEPS = %d \n",STEPS);
 
 double * a;
 double * b;
@@ -47,9 +84,29 @@ for (int n=0; n<LEN; n++)
   //printf("%f----",array1[n]);
 }
 
-//test(array2);
-multimult(a,b,LEN,1);
-test(a);
-// printf("Dealloc Complete");
+// Call function and time it.
+
+// Calculate time taken by Sequential request
+clock_gettime(CLOCK_REALTIME, &requestStart);
+multimult(a,b,LEN,STEPS);
+clock_gettime(CLOCK_REALTIME, &requestEnd);
+
+double accum = ( requestEnd.tv_sec - requestStart.tv_sec )
+  + ( requestEnd.tv_nsec - requestStart.tv_nsec )
+  / BILLION;
+printf( "%lf\n", accum );
+
+// Time taken by Parallel request
+clock_gettime(CLOCK_REALTIME, &requestStart);
+multimult_OMP(a,b,LEN,STEPS);
+clock_gettime(CLOCK_REALTIME, &requestEnd);
+
+double accum_OMP = ( requestEnd.tv_sec - requestStart.tv_sec )
+  + ( requestEnd.tv_nsec - requestStart.tv_nsec )
+  / BILLION;
+printf( "%lf\n", accum_OMP );
+
+free(a);
+free(b);
 
 }
